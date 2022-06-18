e283dc421678ced73a928c881af09fbb0e9daf62
Augie <augie100801@gmail.com>
2022-06-18 14:36:18 +0700

    sementara

---
+++ app/src/main/java/com/ubaya/ubayakuliner160419035/view/Interfaces.kt
@@ -0,0 +1,28 @@
+package com.ubaya.ubayakuliner160419035.view
+
+import android.view.View
+import com.ubaya.ubayakuliner160419035.model.Review
+
+interface ButtonAddReservationClickListener{
+    fun onButtonAddReservationClick(v:View)
+}
+
+interface ButtonToAddReservationClickListener{
+    fun onButtonToAddReservationClick(v:View)
+}
+
+interface ButtonDetailTenantClickListener{
+    fun onButtonDetailTenantClick(v: View)
+}
+
+interface ButtonDetailPromoClickListener{
+    fun onButtonDetailPromoClick(v: View)
+}
+
+interface ButtonWriteReviewClickListener{
+    fun onButtonWriteReviewClick(v: View)
+}
+
+interface ButtonAddReviewClickListener{
+    fun onButtonAddReviewClick(v: View)
+}
